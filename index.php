91ae8043db333a5e9318ddfd6b19a8e295434d1e
Ralph Meira <rmeira@vmware.com>
2020-07-27 22:50:24 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<script type="text/javascript">
+window.onload = function() {
+	var dataPoints = [];
+	var chart; 
+	$.getJSON("https://cutemp.apps.ourpcf.com/user1-fact.apps.ourpcf.com/10000", function(data) {  
+		$.each(data, function(key, value){
+			dataPoints.push({x: value[0], y: parseInt(value[1])});
+		});
+		chart = new CanvasJS.Chart("chartContainer",{
+			title:{
+				text:"Live Line-Chart of HTTP GET Response Time in Milliseconds"
+			},
+			data: [{
+				type: "line",
+				dataPoints : dataPoints,
+			}]
+		});
+		chart.render();
+		updateChart();
+	});
+	function updateChart() {
+	$.getJSON("https://cutemp.apps.ourpcf.com/user1-fact.apps.ourpcf.com/10000", function(data) {
+		$.each(data, function(key, value) {
+			dataPoints.push({
+			x: parseInt(value[0]),
+			y: parseInt(value[1])
+			});
+		});
+		chart.render();
+		setTimeout(function(){updateChart()}, 1000);
+	});
+	}
+}
+</script>
+<script type="text/javascript" src="https://canvasjs.com/assets/script/jquery-1.11.1.min.js"></script>
+<script type="text/javascript" src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+</head>
+<body>
+<div id="chartContainer" style="height: 300px; width: 100%;"></div>
+</body>
+</html>
+
